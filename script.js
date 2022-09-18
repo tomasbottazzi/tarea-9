6e427c8bbdc2995f969d52e85185830f59bb1be3
tomasbottazzi <tomibottazzi@gmail.com>
2022-09-18 18:51:55 -0300

    Esta es la tarea 9

---
+++ script.js
@@ -0,0 +1,11 @@
+var color = prompt("Ingrese un color: ");
+var nombre = prompt("Ingrese un nombre propio: ");
+var edad = prompt("Ingrese una edad: ");
+var color_nombre = color + " de " + nombre;
+
+color = "azul";
+nombre = "juan";
+
+console.log(color, nombre, edad);
+alert(color_nombre);
+
